4686d5d19afb2a4bf94e55046bf729711cb66ef0
Thant Htet Aung <taung@c2r1s1.42singapore.sg>
2024-10-20 22:08:01 +0800

    clean commit of prev

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: taung <taung@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/20 17:09:33 by taung             #+#    #+#             */
-/*   Updated: 2024/10/20 21:51:27 by taung            ###   ########.fr       */
+/*   Updated: 2024/10/20 22:07:11 by taung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,11 @@ int	main(int argc, char *argv[])
 {
 	if (argc == 2)
 	{
-		if (validation_si(argv[1]);)
+		if (validation_si(argv[1]))
+		{
+			//call init
+			//call sort
+		}
 	}
 	else if (argc > 2)
 	{
