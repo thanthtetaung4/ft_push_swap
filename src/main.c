a38ca102b958b57a48f4287fff8e20d80bcd9fad
Thant Htet Aung <taung@c2r5s9.42singapore.sg>
2024-10-21 22:46:48 +0800

    init file created :|

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: taung <taung@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/20 17:09:33 by taung             #+#    #+#             */
-/*   Updated: 2024/10/21 22:41:43 by taung            ###   ########.fr       */
+/*   Updated: 2024/10/21 22:46:22 by taung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,13 +38,11 @@ void	multi_str_ops(char **inputs)
 
 int	main(int argc, char *argv[])
 {
-
 	if (argc == 2)
 		single_str_ops(argv[1]);
 	else if (argc > 2)
 		multi_str_ops(argv);
 	else
 		return (-1);
-
 	return (0);
 }
